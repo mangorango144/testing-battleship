2794b319702937557abe7f7c226bde6d4a90003a
Rango Mango <mangorango144@gmail.com>
2025-01-03 12:59:36 +0200

    refactor: remove redundant test

--- src/__tests__/shipFactory.test.js
+++ src/__tests__/shipFactory.test.js
@@ -1,11 +1,6 @@
 const createShip = require("../factories/shipFactory");
 
 describe("Ship Factory", () => {
-  test("Ship should start with 0 hits", () => {
-    const ship = createShip(3);
-    expect(ship.isSunk()).toBe(false);
-  });
-
   test("hit() should increase the hit count", () => {
     const ship = createShip(3);
     ship.hit();
